f2bbd490e95b83bd0b9c4e8e1046003405a0d510
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-04-09 19:40:21 -0500

    update & refresh

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,17 +5,19 @@ function Footer() {
   return (
     <footer>
       <p>Follow us:</p>
-      <img
-        className="logo"
-        src="/public/images/facebook-logo.png"
-        alt="Facebook"
-      />
-      <img
-        className="logo"
-        src="/public/images/insta-logo.png"
-        alt="Instagram"
-      />
-      <img className="logo" src="/public/images/x-logo.png" alt="X" />
+      <div>
+        <img
+          className="logo"
+          src="/public/images/facebook-logo.png"
+          alt="Facebook"
+        />
+        <img
+          className="logo"
+          src="/public/images/insta-logo.png"
+          alt="Instagram"
+        />
+        <img className="logo" src="/public/images/x-logo.png" alt="X" />
+      </div>
     </footer>
   );
 }
