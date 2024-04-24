288122d5ba837f2de1f5e2b177510d531bbacccf
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-04-24 16:01:19 -0500

    updated Table creation SQL

--- database.sql
+++ database.sql
@@ -2,17 +2,13 @@
 
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "first_name" VARCHAR (50) NOT NULL,
-    "last_name" VARCHAR (50) NOT NULL,
-    "email" VARCHAR (50) UNIQUE NOT NULL,
-    "pass_hash" VARCHAR (1000) NOT NULL, 
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL, 
+    "first_name" VARCHAR (50),
+    "last_name" VARCHAR (50),
     "type" VARCHAR (20), 
     "phone" VARCHAR (10)
 );
-
-INSERT INTO "user" ("first_name", "last_name", "email", "pass_hash")
-VALUES ('Nicole', 'Barnhouse', 'nicolebarnhouse@gmail.com', '$2b$10$uxPm0qeJAz70oqhEg8dX6uXlYc2PWUtPuZhTa65OiDv2LCHA41OLq', 
-'Tracy', 'Tyson', 'ttyson@tca.org', '$2b$10$uxPm0qeJAz70oqhEg8dX6uXlYc2PWUtPuZhTa65OiDv2LCHA41OLq');
 
 CREATE TABLE "teacher" (
     "id" SERIAL PRIMARY KEY,
@@ -22,31 +18,23 @@ CREATE TABLE "teacher" (
     "user_id" INT REFERENCES "user"
 );
 
-INSERT INTO "teacher" ("grade", "room_number", "extension", "user_id")
-VALUES ('5', '206', '101', '2');
-
 CREATE TABLE "substitute_availability" (
     "id" SERIAL PRIMARY KEY,
     "date" Date NOT NULL, 
-    "day" VARCHAR (20) NOT NULL,
+    "type" VARCHAR (20) NOT NULL,
+    "comments" VARCHAR (1000),
     "user_id" INT REFERENCES "user"
 );
 
-INSERT INTO "substitute_availability" ("date", "day", "user_id")
-VALUES ('2024-03-26', 'Tuesday', '1');
-
 CREATE TABLE "requests" (
     "id" SERIAL PRIMARY KEY,
-    "request_start_date" DATE NOT NULL, 
-    "status" VARCHAR (20), 
+    "request_start_date" Date, 
+    "school" VARCHAR(50) DEFAULT 'Tulsa Classical Academy',
+    "status" VARCHAR (20) DEFAULT 'Requested', 
     "teacher_id" INT REFERENCES "teacher", 
-    "submit_date" DATE NOT NULL,
-    "request_end_date" DATE NOT NULL,
+    "user_id" INT REFERENCES "user",
+    "submit_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     "reason" VARCHAR (255),
     "admin_notes" VARCHAR (255),
-    "sub_notes" VARCHAR (255),
-    "attachment_url" VARCHAR (255)
+    "sub_notes" VARCHAR (255)
 );
-
-INSERT INTO "requests" ("submit_date", "request_start_date", "request_end_date", "teacher_id")
-VALUES ('2024-03-26', '2024-03-27', '2024-03-27', '2');
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,17 +6,9 @@ function Footer() {
     <footer>
       <p>Follow us:</p>
       <div>
-        <img
-          className="logo"
-          src="/public/images/facebook-logo.png"
-          alt="Facebook"
-        />
-        <img
-          className="logo"
-          src="/public/images/insta-logo.png"
-          alt="Instagram"
-        />
-        <img className="logo" src="/public/images/x-logo.png" alt="X" />
+        <img className="logo" src="/public/facebook-logo.png" alt="Facebook" />
+        <img className="logo" src="/public/insta-logo.png" alt="Instagram" />
+        <img className="logo" src="/public/x-logo.png" alt="X" />
       </div>
     </footer>
   );
